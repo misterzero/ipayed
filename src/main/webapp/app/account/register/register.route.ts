feb1c6a95688b8dc52e69aa2ad6e8be319146532
ryanhill <rhill@ippon.fr>
2018-04-16 16:27:18 -0400

    finally able to merge front end into working branch, jhi (translation) issues working now

--- src/main/webapp/app/account/register/register.route.ts
+++ src/main/webapp/app/account/register/register.route.ts
@@ -3,10 +3,10 @@ import { Route } from '@angular/router';
 import { RegisterComponent } from './register.component';
 
 export const registerRoute: Route = {
-    path: 'register',
+    path: 'register/:userType',
     component: RegisterComponent,
     data: {
         authorities: [],
-        pageTitle: 'Registration'
+        pageTitle: 'register.title'
     }
 };
--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 import { JhiEventManager } from 'ng-jhipster';
+import { Router } from '@angular/router';
 
 import { Account, LoginModalService, Principal } from '../shared';
 
@@ -19,7 +20,8 @@ export class HomeComponent implements OnInit {
     constructor(
         private principal: Principal,
         private loginModalService: LoginModalService,
-        private eventManager: JhiEventManager
+        private eventManager: JhiEventManager,
+        private router: Router
     ) {
     }
 
@@ -44,5 +46,13 @@ export class HomeComponent implements OnInit {
 
     login() {
         this.modalRef = this.loginModalService.open();
+    }
+
+    registerUser() {
+        this.router.navigate(['register/user']);
+    }
+
+    registerBusiness() {
+        this.router.navigate(['register/business']);
     }
 }
